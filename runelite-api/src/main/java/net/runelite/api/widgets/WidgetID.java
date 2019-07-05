a3afe779ea4686cd00375fc95c542481287e6be2
Theo <purplecatfishz@aol.com>
2019-07-04 18:04:22 -1000

    Test

--- runelite-api/src/main/java/net/runelite/api/widgets/WidgetID.java
+++ runelite-api/src/main/java/net/runelite/api/widgets/WidgetID.java
@@ -821,12 +821,11 @@ public class WidgetID
 		static final int SAFE_ZONE_CONTAINER = 31;
 	}
 
-	static class TradeScreen
-	{
-		static final int ACCEPT = 12;
-		static final int DECLINE = 15;
-		static final int TRADING_WITH = 31;
-		static final int TRADING_WITH_INVENTORY_SLOTS = 9;
-	}
-
+    static class TradeScreen
+    {
+        static final int ACCEPT = 12;
+        static final int DECLINE = 15;
+        static final int TRADING_WITH = 31;
+        static final int TRADING_WITH_INVENTORY_SLOTS = 9;
+    }
 }
