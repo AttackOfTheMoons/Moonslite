2da72895173e63720f37e57b5577ca36120bac28
Theo <purplecatfishz@aol.com>
2019-07-18 11:16:21 -1000

    antiDragFix

--- runelite-client/src/main/java/net/runelite/client/plugins/antidrag/AntiDragConfig.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/antidrag/AntiDragConfig.java
@@ -45,7 +45,7 @@ public interface AntiDragConfig extends Config
 	@ConfigItem(
 			keyName = "alwaysOn",
 			name = "Always On",
-			description = "Enable Anti Drag Always (press shift to refresh)",
+			description = "Enable Anti Drag Always",
 			position = 2
 	)
 	default boolean alwaysOn()
